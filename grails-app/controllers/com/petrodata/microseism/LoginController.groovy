a3ff99356962714215e430da80c2d9cfd9a11757
xp <13910818313@163.com>
2016-04-16 11:34:50 +0800

    login

--- grails-app/controllers/com/petrodata/microseism/LoginController.groovy
+++ grails-app/controllers/com/petrodata/microseism/LoginController.groovy
@@ -1,11 +1,21 @@
 package com.petrodata.microseism
 
+import grails.converters.JSON
+
 class LoginController {
 
     def index() {
 
     }
     def clientLogin(){
-        MsUser.find
+        def map=[:]
+        def user=MsUser.findByUserName(params.username);
+        if(user.userPwd==params.password){
+            map.result=true;
+            map.id=user.id;
+        }else{
+            map.result=false;
+        }
+        render map as JSON;
     }
 }
